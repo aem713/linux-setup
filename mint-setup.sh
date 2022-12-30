6eaa41aae37248b7766204eb714a11c8b4777cd1
aem713 <amotu15@gmail.com>
2022-12-30 00:25:27 -0600

    Uncomment QEMU-KVM

--- mint-setup.sh
+++ mint-setup.sh
@@ -12,13 +12,13 @@
 
 # 4) Install and set up Nala
 sudo apt install nala -y
-printf '1 2 3 4\ny\n' | sudo nala fetch
+printf '1\ny\n' | sudo nala fetch
 
 # 5) Update and Upgrade system
 sudo nala upgrade -y
 
 # 6) Install all needed packages
-sudo nala install -y vim neofetch v4l2loopback-dkms ffmpeg obs-studio gimp shotcut spotify-client git gnuradio gparted hwloc
+sudo nala install -y vim v4l2loopback-dkms ffmpeg obs-studio gimp shotcut spotify-client gnuradio gparted hwloc
 
 # 7) Download all needed .deb files
 wget -c --content-disposition -i deb.txt
@@ -44,31 +44,33 @@ EOF
 fi
 
 # 11) Install auto-cpufreq
+cd ..
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && printf 'i\n' | sudo ./auto-cpufreq-installer
 sudo auto-cpufreq --install
 cd ..
 
 # 12) Set up QEMU-KVM
-#if [ $(egrep -c '(vmx|svm)' /proc/cpuinfo) -gt 0 ]; then
-#    sudo nala install -y qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager
-#    sudo virsh net-start default
-#    sudo virsh net-autostart default
-#    sudo virsh net-list --all
-#    newgrp libvirt # Allows user to join libvirt group without logout
-#    sudo usermod -aG libvirt $USER
-#    sudo usermod -aG libvirt-qemu $USER
-#    sudo usermod -aG kvm $USER
-#    sudo usermod -aG input $USER
-#    sudo usermod -aG disk $USER
-#    # Also set up VM via virsh
-#else
-#    echo "Virtualization is not enabled in UEFI"
-#fi
+if [ $(egrep -c '(vmx|svm)' /proc/cpuinfo) -gt 0 ]; then
+    sudo nala install -y qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager
+    sudo nala install -y qemu-system-x86 libvirt-clients libvirt-daemon-system libvirt-daemon-config-network bridge-utils virt-manager ovmf
+    sudo virsh net-start default
+    sudo virsh net-autostart default
+    sudo virsh net-list --all
+    newgrp libvirt # Allows user to join libvirt group without logout
+    sudo usermod -aG libvirt $USER
+    sudo usermod -aG libvirt-qemu $USER
+    sudo usermod -aG kvm $USER
+    sudo usermod -aG input $USER
+    sudo usermod -aG disk $USER
+    # Also set up VM via virsh
+else
+    echo "Virtualization is not enabled in UEFI"
+fi
 
 sudo mkdir /var/lib/libirt/images/isos
-sudo wget -cP /var/lib/libvirt/images/isos https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
-sudo wget -cP /var/lib/libvirt/images/isos https://software.download.prss.microsoft.com/dbazure/Win10_22H2_English_x64.iso?t=7ba00706-ef7a-4923-8f53-bd4b65fc75aa&e=1672445294&h=1ac5458900406bdd047d283039b3a18a2dc574e2583d49cf8f01a2ec373daf26
+#sudo wget -cP /var/lib/libvirt/images/isos https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+#sudo wget -cP /var/lib/libvirt/images/isos https://software.download.prss.microsoft.com/dbazure/Win10_22H2_English_x64.iso?t=7ba00706-ef7a-4923-8f53-bd4b65fc75aa&e=1672445294&h=1ac5458900406bdd047d283039b3a18a2dc574e2583d49cf8f01a2ec373daf26
 
 # Future script for automated creation of the VM
 # Download hwloc
