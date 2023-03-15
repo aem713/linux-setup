9ea565f65a6815e3662664edb44863bbf24e9c81
aem713 <amotu15@gmail.com>
2023-03-14 23:07:22 -0400

    Renumber Fedora

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -6,36 +6,35 @@ sudo sh -c 'echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf'
 sudo sh -c 'echo "defaultyes=True" >> /etc/dnf/dnf.conf'
 sudo sh -c 'echo "keepcache=True" >> /etc/dnf/dnf.conf'
 
-# 2) Update dnf
+# 2) Update and upgrade dnf
 sudo dnf update
+sudo dnf upgrade
 
 # 3) Enable RPM Fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# 4) Allow installation of RPM Fusion packages via GUI
+# 4) Allow installation of RPM Fusion packages via GUI (Appsteam data)
 sudo dnf groupupdate core
 
-#5) Add LibreWolf Repo
+# 5) Add LibreWolf Repo
 sudo dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
 
-#6) Import VS Code GPG Key
+# 6) Import VS Code GPG Key and VS Code repository
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc 
-
-#7) Import VS Code repository
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-#6) Install packages
+# 7) Install packages
 sudo dnf install -y vim git vlc gnuradio neofetch librewolf gimp lpf-spotify-client gparted discord code
 
-#7) Install Spotify
+# 8) Install Spotify
 lpf update
 
-#8) Install VS Code Extensions
+# 9) Install VS Code Extensions
 code --install-extension jdinhlife.gruvbox
 code --install-extension vscodevim.vim
 
 
-# 4) Create Aliases
+# 10) Create Aliases
 if [ -e ~/.bashrc.d/aliases ]; then
     echo "File ~/.bashrc.d/aliases already exists!"
 else
@@ -47,31 +46,31 @@ ck () { cd $@ && clear && ls; }
 EOF
 fi
 
-# 5) Enable Flathub
+# 11) Enable Flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# 6) Change hostname
+# 12) Change hostname
 sudo hostnamectl set-hostname fedora
 
-# 7) Install media codecs
+# 13) Install media codecs
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf groupupdate sound-and-video
 
-# 8) Boost by having cache
+# 14) Boost by having cache
 sudo dnf copr enable elxreno/preload -y && sudo dnf install preload -y
 
-# 9) Install Virtualization
+# 15) Install Virtualization
 sudo dnf install @virtualization
 
-# 10) Start libvirtd
+# 16) Start libvirtd
 sudo systemctl start libvirtd
 sudo systemctl enable libvirtd
 lsmod | grep kvm
 
-# 11) Make isos directory
+# 17) Make isos directory
 sudo mkdir /var/lib/libvirt/images/isos
 
-# 11) Install auto-cpufreq
+# 18) Install auto-cpufreq
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && printf 'i\n' | sudo ./auto-cpufreq-installer
 sudo auto-cpufreq --install
@@ -81,3 +80,7 @@ cd ..
 # sudo dnf remove abc
 # sudo dnf autoremove (removes orphaned packages which are dependancies of removed packages)
 # sudo dnf clean packages (removes cached packages)
+
+# How To Set Up Git
+# git config --global user.name $USERNAME
+# git config --global user.email $EMAIL
